cd1b210df82042546b0dc80f1d405a2b5725456c
Konnkeryou <83404617+Konnkeryou@users.noreply.github.com>
2021-07-13 13:35:32 +1000

    added loop function
    Loop function to re-play  the game again

--- Terminal Application.py
+++ Terminal Application.py
@@ -21,3 +21,42 @@ print("What's Good? " + name + "! Best of Luck! " )
 time.sleep(3)
 print("Let the Games Begin!\n")
 time.sleep(5)
+
+# creating the main functions 
+
+def main():
+    global count
+    global display 
+    global word
+    global already_guessed 
+    global length 
+    global play_game 
+    words_to_guess = [
+        "ivy", "cycle", "quiz", "staff", "gossip", "unknown", "jackpot", "flyby", "joyful", "nine"
+    ]
+    word =  random.choice(words_to_guess)
+    length= len(word)
+    count= 0
+    display= '_' * length
+    already_guessed = []
+    play_game= ""
+
+# loop to play the game again 
+
+def play_loop():
+    global play_game
+    play_game = input( "Play Again?  y= yes, n= no \n")
+    while play_game not in ("Y", "N", "y", "n"):
+        play_game = input( "Play Again?  y= yes, n= no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Sad to see you go... Catch ya next time!!")
+        exit()
+
+
+
+
+
+
+

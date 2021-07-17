3e6858e5e4bdcba5ffa4694db5ad71f7fccc07de
Konnkeryou <83404617+Konnkeryou@users.noreply.github.com>
2021-07-17 12:29:43 +1000

    Update Terminal Application.py

--- Terminal Application.py
+++ Terminal Application.py
@@ -19,6 +19,51 @@ time.sleep(3)
 print("Let the Games Begin!!")
 time.sleep(3)
 
+
+words = ' ivy console camera awesome mother coding python warzone basketball earth future computer artificial coffee speaker mirror abstract glass moonlight'.split() 
+#Random function 
+def get_random_word(word_list):
+    word_index = random.randint (0, len(word_list) - 1)
+    return word_list[word_index]
+
+#Displaying the hangman graphics
+def display_board(missed_letters, correct_letters, secret_word): 
+    print(HANGINGMAN_GRAPHIC[len(missed_letters)])
+    print()
+
+    print ('Missed letters:', end =' ')
+    for letter in missed_letters:
+        print(letter, end=' ')
+        print()
+
+#placing correctly guessed words in the spaces 
+    blanks = '_' * len(secret_word)
+    
+    for i in range(len(secret_word)):
+        if secret_word[i] in correct_letters:
+            blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
+
+#Displaying the hangman word with spaces in between the letters
+    for letter in blanks: 
+        print(letter, end=' ' )
+        print()
+
+#Error checking, making sure players enter a single letter
+def get_guess (already_guessed):
+    while True: 
+        print('Pick a letter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1: 
+            print('Please try a single letter. ')
+        elif guess in already_guessed:
+            print ('Nope! you have already guessed that mane. Try another letter')
+        elif guess not in 'abcdefghijklmnoqrstuvwxyz':
+            print('Please enter a LETTER')
+        else: 
+            return guess  
+
+# the hangman graphic
 HANGINGMAN_GRAPHIC = [ '''
     +--------+
     | 
@@ -84,49 +129,6 @@ HANGINGMAN_GRAPHIC = [ '''
  ___|____
 |________| ''',]
 
-words_to_guess = ' ivy console camera awesome mother coding python warzone basketball earth future computer artificial coffee speaker mirror abstract glass moonlight'.split() 
-#Random function 
-def get_randomWord(word_list):
-    word_index = random.randit (0, len(word_list) - 1)
-    return word_list[word_index]
-
-#Displaying the hangman graphics
-def display_board(missed_letters, correct_letters, secret_word): 
-    print(HANGINGMAN_GRAPHIC[len(missed_letters)])
-    print()
-
-    print ('Missed letters:', end =' ')
-    for letter in missed_letters:
-        print(letter, end=' ')
-        print()
-
-    blank = '_' * len(secret_word)
-
-#placing correctly guessed words in the spaces 
-    for i in range(len(secret_word)):
-        if secret_word[i] in correct_letters:
-            blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
-
-#Displaying the hangman word with spaces in between the letters
-    for letter in blanks: 
-        print(letter, end=' ' )
-        print()
-
-#Error checking, making sure players enter a single letter
-def get_guess (already_guessed):
-    while True: 
-        print('Pick a letter.')
-        guess = input()
-        guess = guess.lower()
-        if len(guess) != 1: 
-            print('Please try a single letter. ')
-        elif guess in already_guessed:
-            print ('Nope! you have already guessed that mane. Try another letter')
-        elif guess not in 'abcdefghijklmnoqrstuvwxyz':
-            print('Please enter a LETTER')
-        else: 
-            return guess  
-
 # Loops to restart the Game:
 
 def play_loop():
@@ -136,7 +138,7 @@ def play_loop():
 print('HANGMAN')
 missed_letters = ''
 correct_letters = ''
-secret_word = get_randomWord(words_to_guess)
+secret_word = get_random_word(words)
 game_is_done = False 
 
 while True:
@@ -162,3 +164,13 @@ while True:
             display_board(missed_letters, correct_letters, secret_word)
             print('Ahh Sorry mate, You have run out of guesses!\nAfter ' + str(len(missed_letters)) + ' missed guesses and ' + str(len(correct_letters)) + ' correct guesses, the word was " ' + secret_word + '"')
             game_is_done = True 
+#ask player if they wanna restart the game 
+    if game_is_done:
+        if play_loop():
+            missed_letters = ''
+            correct_letters = ''
+            game_is_done = False
+            secret_word = get_random_word(words)
+        else: 
+            break
+

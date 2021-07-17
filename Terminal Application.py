2a503e1abf07dab4a233e63efe13abb962ef3960
Konnkeryou <83404617+Konnkeryou@users.noreply.github.com>
2021-07-17 13:14:59 +1000

    cleaning up overall code

--- Terminal Application.py
+++ Terminal Application.py
@@ -23,45 +23,46 @@ time.sleep(3)
 words = ' ivy console camera awesome mother coding python warzone basketball earth future computer artificial coffee speaker mirror abstract glass moonlight'.split() 
 #Random function 
 def get_random_word(word_list):
-    word_index = random.randint (0, len(word_list) - 1)
+    word_index = random.randint(0, len(word_list) - 1)
     return word_list[word_index]
 
 #Displaying the hangman graphics
-def display_board(missed_letters, correct_letters, secret_word): 
-    print(HANGINGMAN_GRAPHIC[len(missed_letters)])
-    print()
+def display_board(missed_letters, correct_letters, secret_word):
+     print(HANGINGMAN_GRAPHIC[len(missed_letters)])
+     print()
 
-    print ('Missed letters:', end =' ')
-    for letter in missed_letters:
-        print(letter, end=' ')
-        print()
+     print('Missed letters:', end=' ')
+     for letter in missed_letters:
+         print(letter, end=' ')
+     print()
+
 
 #placing correctly guessed words in the spaces 
-    blanks = '_' * len(secret_word)
-    
-    for i in range(len(secret_word)):
-        if secret_word[i] in correct_letters:
-            blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
+     blanks = '_' * len(secret_word)
+
+     for i in range(len(secret_word)):
+         if secret_word[i] in correct_letters:
+             blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
 
 #Displaying the hangman word with spaces in between the letters
-    for letter in blanks: 
-        print(letter, end=' ' )
-        print()
+     for letter in blanks: 
+         print(letter, end=' ')
+     print()
 
 #Error checking, making sure players enter a single letter
-def get_guess (already_guessed):
-    while True: 
-        print('Pick a letter.')
-        guess = input()
-        guess = guess.lower()
-        if len(guess) != 1: 
-            print('Please try a single letter. ')
-        elif guess in already_guessed:
-            print ('Nope! you have already guessed that mane. Try another letter')
-        elif guess not in 'abcdefghijklmnoqrstuvwxyz':
-            print('Please enter a LETTER')
-        else: 
-            return guess  
+def get_guess(already_guessed):
+     while True:
+         print('Pick a letter.')
+         guess = input()
+         guess = guess.lower()
+         if len(guess) != 1:
+             print('Please enter a single letter.')
+         elif guess in already_guessed:
+             print ('Nope! you have already guessed that mane. Try another letter')
+         elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+             print('Please enter a LETTER.')
+         else:
+             return guess
 
 # the hangman graphic
 HANGINGMAN_GRAPHIC = [ '''
@@ -148,6 +149,7 @@ while True:
 
     if guess in secret_word:
         correct_letters = correct_letters + guess
+
 #checks if the player has won
         found_all_letters = True
         for i in range(len(secret_word)): 
@@ -162,9 +164,10 @@ while True:
 
         if len(missed_letters) == len(HANGINGMAN_GRAPHIC) - 1:
             display_board(missed_letters, correct_letters, secret_word)
-            print('Ahh Sorry mate, You have run out of guesses!\nAfter ' + str(len(missed_letters)) + ' missed guesses and ' + str(len(correct_letters)) + ' correct guesses, the word was " ' + secret_word + '"')
+            print('Ahh Sorry mate, You have run out of guesses!\nAfter ' + str(len(missed_letters)) + ' missed guesses and ' + str(len(correct_letters)) + ' correct guesses, the word was "' + secret_word + '"')
             game_is_done = True 
-#ask player if they wanna restart the game 
+
+#ask player if they wanna restart the game  
     if game_is_done:
         if play_loop():
             missed_letters = ''
